7f2ca50473e785e3b01f5027be9ef0fc447b1340
ayeshaarman111 <ayeshaqasim111@gmail.com>
2024-12-23 20:14:45 +0500

    push changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import AOS from 'aos'
-import "aos/dist/aos.css";
+import  "aos/dist/aos.css";
 import { useEffect } from "react";
 
 
